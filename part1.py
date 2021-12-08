8646c277b8c64ef73497d27540ad50f4ee7998d7
Eilean Faltin <94643871+eileanrush@users.noreply.github.com>
2021-12-08 16:48:38 +0000

    did part 2

--- part1.py
+++ part1.py
@@ -21,7 +21,11 @@ smallest = number
 
 number = int(input("Enter another number: "))
 
+number = int(input("Enter another number: "))
+
 if number < smallest:
   smallest = number
-  
+elif number < smallest:
+  smallest = number
+
 print("The smallest number is ", smallest)
